0a353a52a16141bd3c5329f9ed435c7e20c56aea
catsout <outline941@live.com>
2022-02-04 16:04:03 +0800

    mpv: fix compiling on new version

--- src/backend/mpv/mpvbackend.cpp
+++ src/backend/mpv/mpvbackend.cpp
@@ -57,7 +57,7 @@ namespace {
     }
 
     int CreateMpvContex(mpv_handle *mpv, mpv_render_context **mpv_gl) {
-        mpv_opengl_init_params gl_init_params{get_proc_address_mpv, nullptr, nullptr};
+        mpv_opengl_init_params gl_init_params{get_proc_address_mpv, nullptr};
         mpv_render_param params[]{
             {MPV_RENDER_PARAM_API_TYPE, const_cast<char *>(MPV_RENDER_API_TYPE_OPENGL)},
             {MPV_RENDER_PARAM_OPENGL_INIT_PARAMS, &gl_init_params},
