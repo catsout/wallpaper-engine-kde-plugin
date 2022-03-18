94d2eaa3455cac6f9a98d71f84962e851844b6c7
catsout <outline941@live.com>
2022-03-18 11:06:26 +0800

    scene: puppet animation interpolation

--- src/backend_scene/wallpaper/WPPuppet.cpp
+++ src/backend_scene/wallpaper/WPPuppet.cpp
@@ -1,7 +1,16 @@
 #include "WPPuppet.hpp"
+#include <cmath>
+#include "Utils/Logging.h"
 
 using namespace wallpaper;
 using namespace Eigen;
+
+static Quaternionf ToQuaternion(Vector3f euler) {
+        const std::array<Vector3f, 3> axis {Vector3f::UnitX(),Vector3f::UnitY(),Vector3f::UnitZ()};
+        return AngleAxis<float>(euler.z(), axis[2]) *
+            AngleAxis<float>(euler.y(), axis[1]) *
+            AngleAxis<float>(euler.x(), axis[0]);
+};
 
 void WPPuppet::prepared() {
     std::vector<Affine3f> combined_tran(bones.size());
@@ -11,9 +20,20 @@ void WPPuppet::prepared() {
             * b.transform;
         
         b.offset_trans = combined_tran[i].inverse();
+        /*        
         b.world_axis_x = (b.offset_trans.linear() * Vector3f::UnitX()).normalized();
         b.world_axis_y = (b.offset_trans.linear() * Vector3f::UnitY()).normalized();
         b.world_axis_z = (b.offset_trans.linear() * Vector3f::UnitZ()).normalized();
+        */
+    }
+    for(auto& anim:anims) {
+        anim.frame_time = 1.0f / anim.fps;
+        anim.max_time = anim.length / anim.fps;
+        for(auto& b:anim.bframes_array) {
+            for(auto& f:b.frames) {
+                f.quaternion = ToQuaternion(f.angle);
+            }
+        }
     }
 }
 
@@ -24,19 +44,23 @@ Span<Eigen::Affine3f> WPPuppet::genFrame(std::vector<AnimationLayer>& layers, do
     std::transform(layers.rbegin(), layers.rend(), m_layers_in.rbegin(), [&blend, time, this](auto& layer) {
         float cur_blend {0.0f};
         auto it = std::find_if(anims.begin(), anims.end(), [&layer](auto& a) { return layer.id == a.id; });
+        bool ok = it != anims.end() && layer.visible;
+        Animation::InterpolationInfo info;
 
-        if(layer.visible) {
+        if(ok) {
             cur_blend = blend * layer.blend;
             blend *= 1.0f - layer.blend;
             blend = blend < 0.0f ? 0.0f : blend;
 
-            if(it != anims.end()) layer.nextFrame(time * layer.rate, *it);
+            layer.cur_time += time * layer.rate;
+            info = it->getInterpolationInfo(&(layer.cur_time));
         }
 
         return AnimationLayer_in {
             .layer = &layer,
             .blend = cur_blend,
-            .anim = it == anims.end() || !layer.visible ? nullptr : &(*it)
+            .anim = ok ? &(*it) : nullptr,
+            .interp_info = info
         };
     });
 
@@ -52,26 +76,24 @@ Span<Eigen::Affine3f> WPPuppet::genFrame(std::vector<AnimationLayer>& layers, do
 
         Vector3f trans {Vector3f::Zero()};
         Vector3f scale {Vector3f::Zero()};
-        auto linner = affine.linear();
+        Quaternionf quat {Quaternionf::Identity()};
 
+        double cur_blend {0.0f};
         for(auto& layer:m_layers_in) {
             if(layer.anim == nullptr || !layer.layer->visible) continue;
             assert(i<layer.anim->bframes_array.size());
             if(i>=layer.anim->bframes_array.size()) continue;
-            auto iframe = layer.layer->curFrame();
-            auto& frame = layer.anim->bframes_array[i].frames[iframe];
 
-            trans += layer.blend * frame.position;// - bone.position);
-            const std::array<Vector3f, 3> axis {Vector3f::UnitX(),Vector3f::UnitY(),Vector3f::UnitZ()};
-            //const std::array<Vector3f, 3> axis {bone.world_axis_x, bone.world_axis_y, bone.world_axis_z};
-            linner *= AngleAxis<float>(layer.blend * frame.angle[2], axis[2]).matrix();
-            linner *= AngleAxis<float>(layer.blend * frame.angle[1], axis[1]).matrix();
-            linner *= AngleAxis<float>(layer.blend * frame.angle[0], axis[0]).matrix();
-            for(uint i=0;i<3;i++) {
-                scale[i] += layer.blend * frame.scale[i];
-            }
+            auto& info = layer.interp_info;
+            auto& frame_a = layer.anim->bframes_array[i].frames[info.frame_a];
+            auto& frame_b = layer.anim->bframes_array[i].frames[info.frame_b];
+            double one_t = 1.0f - info.t;
+            quat = frame_a.quaternion.slerp(info.t, frame_b.quaternion).slerp(1.0f - layer.blend, quat);
+            trans += layer.blend * (frame_a.position * one_t + frame_b.position * info.t);
+            scale += layer.blend * (frame_a.scale * one_t + frame_b.scale * info.t);
         }
         affine.pretranslate(trans);
+        affine.rotate(quat);
         affine.scale(scale);
         affine = parent * affine;
     }
@@ -80,4 +102,29 @@ Span<Eigen::Affine3f> WPPuppet::genFrame(std::vector<AnimationLayer>& layers, do
         m_final_affines[i] *= bones[i].offset_trans.matrix();
     }
     return m_final_affines;
+}
+
+static constexpr void genInterpolationInfo(WPPuppet::Animation::InterpolationInfo& info, double& cur, uint length, double frame_time, double max_time) {
+    cur = std::fmod(cur, max_time);
+    double _rate = cur / frame_time;
+
+    info.frame_a = ((uint)_rate) % length;
+    info.frame_b = (info.frame_a+1) % length;
+    info.t = _rate - info.frame_a;
+}
+
+WPPuppet::Animation::InterpolationInfo WPPuppet::Animation::getInterpolationInfo(double* cur_time) const {
+    InterpolationInfo _info;
+    auto& _cur_time = *cur_time;
+
+    if(mode == PlayMode::Loop || mode == PlayMode::Single) {
+        genInterpolationInfo(_info, _cur_time, length, frame_time, max_time);
+    } else if(mode == PlayMode::Mirror) {
+        const auto _get_frame = [this](auto f) { return f >= length ? (length-1) - (f-length) : f; };
+        genInterpolationInfo(_info, _cur_time, length*2.0f, frame_time, max_time*2.0f);
+        _info.frame_a = _get_frame(_info.frame_a);
+        _info.frame_b = _get_frame(_info.frame_b);
+    }
+
+    return _info;
 }
--- src/backend_scene/wallpaper/WPPuppet.hpp
+++ src/backend_scene/wallpaper/WPPuppet.hpp
@@ -20,14 +20,19 @@ public:
         bool noParent() const { return parent == 0xFFFFFFFFu; }
         // prepared
         Eigen::Affine3f offset_trans { Eigen::Affine3f::Identity() };
+        /*
         Eigen::Vector3f world_axis_x;
         Eigen::Vector3f world_axis_y;
         Eigen::Vector3f world_axis_z;
+        */
     };
     struct BoneFrame {
         Eigen::Vector3f position;
         Eigen::Vector3f angle;
         Eigen::Vector3f scale;
+
+        // prepared
+        Eigen::Quaternionf quaternion;
     };
     struct Animation {
         uint32_t id;
@@ -38,6 +43,16 @@ public:
 
         struct BoneFrames { std::vector<BoneFrame> frames; };
         std::vector<BoneFrames> bframes_array;
+
+        // prepared
+        float max_time;
+        float frame_time;
+        struct InterpolationInfo {
+            uint32_t frame_a;
+            uint32_t frame_b;
+            double t;
+        };
+        InterpolationInfo getInterpolationInfo(double* cur_time) const;
     };
 
     struct AnimationLayer {
@@ -46,43 +61,7 @@ public:
         float blend {1.0f};
         bool visible {true};
 
-        uint resolveModeIndex(uint i, int step, const Animation& anim) {
-            uint res {0};
-            if(anim.mode == PlayMode::Mirror) {
-                if(m_mirror_b) {
-                    res = std::abs((int)i-step) % anim.length;
-                    if(i < step) {
-                        m_mirror_b = false;
-                    }
-                } else {
-                    res = i+step;
-                    if(i+step > anim.length) {
-                        res = anim.length - (res % anim.length);
-                        m_mirror_b = true;
-                    }
-                }
-            }
-            else {
-                res = (i+step) % anim.length;
-            }
-            return res;
-        }
-
-        uint nextFrame(double time, const Animation& anim) {
-            time += m_remain_time;
-            double frame_time = (1.0f / anim.fps);
-            uint n = time / frame_time;
-
-            m_cur_frame = resolveModeIndex(m_cur_frame, n, anim);
-
-            m_remain_time = time - n*frame_time;
-            return m_cur_frame;
-        }
-        uint curFrame() const { return m_cur_frame; };
-    private:
-        bool m_mirror_b {false};
-        uint m_cur_frame {0};
-        double m_remain_time {0};
+        double cur_time {0.0f};
     };
 public:
     std::vector<Bone> bones;
@@ -92,9 +71,10 @@ public:
     void prepared();
 private:
     struct AnimationLayer_in {
-        const AnimationLayer* layer;
+        AnimationLayer* layer;
         float blend;
         Animation* anim {nullptr};
+        Animation::InterpolationInfo interp_info;
     };
     std::vector<Eigen::Affine3f> m_final_affines;
     std::vector<AnimationLayer_in> m_layers_in;
