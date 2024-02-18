catsout/wallpaper-engine-kde-plugin

commit de11e8b3201cf4e285908e9ba4d108fff591d3d7

Fix settings page
Plasma 6 dropped support of reading metadata.desktop,
so converted to metadata.json (`desktoptojson -i <file>`,
as suggested by configuration step with Plasma 5)
with minor edits.
Also adapted to new APIs in QML files.

TODO: Icons in settings page is missing.
Signed-off-by: fxzjshm <fxzjshm@163.com>

plugin/contents/ui/page/WallpaperPage.qml
@@ -2,7 +2,7 @@ import QtQuick 2.6
 import QtQuick.Controls 2.3
 import QtQuick.Controls 2.3 as QQC
 import QtQuick.Window 2.0 // for Screen
-import QtQuick.Dialogs 1.2
+import QtQuick.Dialogs
 import QtQuick.Layouts 1.5
 
 import ".."
@@ -14,7 +14,7 @@ import "../js/bbcode.mjs" as BBCode
 import org.kde.plasma.core 2.0 as PlasmaCore
 import org.kde.plasma.components 3.0 as PlasmaComponents
 // for kcm gridview
-import org.kde.kcm 1.1 as KCM
+import org.kde.kcmutils as KCM
 import org.kde.kirigami 2.6 as Kirigami
 import org.kde.kquickcontrolsaddons 2.0
 
@@ -205,11 +205,11 @@ RowLayout {
                         ]
                         thumbnail: Rectangle {
                             anchors.fill: parent
-                            QIconItem {
+                            Kirigami.Icon {
                                 anchors.centerIn: parent
                                 width: root.iconSizes.large
                                 height: width
-                                icon: "view-preview"
+                                source: "view-preview"
                                 visible: !imgPre.visible
                             }
                             Image {
@@ -297,12 +297,9 @@ RowLayout {
                 }
             }
 
-            FileDialog {
+            FolderDialog {
                 id: wpDialog
                 title: "Select steamlibrary folder"
-                selectFolder: true
-                selectMultiple : false
-                nameFilters: [ "All files (*)" ]
                 onAccepted: {
                     const path = Utils.trimCharR(wpDialog.fileUrls[0], '/');
                     cfg_SteamLibraryPath = path;

plugin/contents/ui/components/IconSvg.qml
@@ -1,5 +1,5 @@
 import QtQuick 2.6
-import QtGraphicalEffects 1.12
+import Qt5Compat.GraphicalEffects
 
 
 Item {
@@ -8,7 +8,7 @@ Item {
 
     property alias source: image.source
     property alias fillMode: image.fillMode
-    property alias smooth: image.smooth
+    // property alias smooth: image.smooth
     property alias color: overlay_color.color
 
     Image {

commit 50d350f5a2033db6e28a62712558edbb884b8fb9

Fix Steam library selection
Reported by @Logical-sh  at
https://github.com/catsout/wallpaper-engine-kde-plugin/pull/344#issuecomment-1950961550

Fixes: de11e8b ("Fix settings page")
Signed-off-by: fxzjshm <fxzjshm@163.com>

plugin/contents/ui/page/WallpaperPage.qml
@@ -301,7 +301,7 @@ RowLayout {
                 id: wpDialog
                 title: "Select steamlibrary folder"
                 onAccepted: {
-                    const path = Utils.trimCharR(wpDialog.fileUrls[0], '/');
+                    const path = Utils.trimCharR(wpDialog.selectedFolder.toString(), '/');
                     cfg_SteamLibraryPath = path;
                     wpListModel.refresh();
                 }